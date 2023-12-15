53709d186a9344dcd507760a4eb40479287610d7
Leonardo Redin <redin@hey.com>
2023-12-14 23:36:52 -0300

    user service

--- src/modules/user/services/user.service.ts
+++ src/modules/user/services/user.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
 import { UserRepository } from '../repositories/user.repository';
+import { CreateUserDTO, UpdateUserDTO } from '../dtos/user-dtos';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: UserRepository,
+  ) {}
 
-  // Write services
+  async createUser(createUser: CreateUserDTO): Promise<User> {
+    return await this.userRepository.createUser(createUser);
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    return await this.userRepository.getAllUsers();
+  }
+
+  async getUserById(id: number): Promise<User> {
+    const user = await this.userRepository.getUserById(id);
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    return user;
+  }
+
+  async updateUser(updateUser: UpdateUserDTO): Promise<User> {
+    const updatedUser = await this.userRepository.updateUser(updateUser);
+
+    if (!updatedUser) {
+      throw new NotFoundException(`User with ID ${updateUser.id} not found`);
+    }
+
+    return updatedUser;
+  }
+
+  async deleteUser(id: number): Promise<void> {
+    const user = await this.userRepository.getUserById(id);
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    await this.userRepository.deleteUser(id);
+  }
 }
